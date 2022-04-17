aa1dc6a686f259e28b4f1cb6f37fffa9c4d3447e
Matteo Besio <matteo.besio@gmail.com>
2022-04-16 19:52:44 -0700

    added all the controllers

--- src/main/controllers/index.ts
+++ src/main/controllers/index.ts
@@ -1,19 +1,99 @@
-import { userModel} from '../models'
-// import { User, Chat, Message } from '../models'
-// import { IDBUser } from '../types'
+import { userModel, chatModel, messageModel } from '../models'
+import { User } from '../models/types'
 
 
-// const getAllUsers = async () => {
-//   const users = await User.findAll()
-//   console.log('users ', users)
-// }
+export const getAllUsers = async () => {
+  const users = await userModel.findAll()
+  if (!users) {
+    throw new Error('An error occured querying users')
+  }
+  return users
+}
 
 export const getUserById = async (id) => {
-  console.log('id', id)
   const user = await userModel.findByPk(id)
-  console.log('user', user)
   if (!user) {
     throw new Error('User does not exist')
   }
   return user
+}
+
+export const getAllChats = async () => {
+  const chats = await chatModel.findAll()
+  if (!chats) {
+    throw new Error('An error occured querying chats')
+  }
+  return chats
+}
+
+export const getChatsById = async (id) => {
+  const chat = await chatModel.findByPk(id)
+  if (!chat) {
+    throw new Error('Chat does not exist')
+  }
+  return chat
+}
+
+export const getAllMessagesFromChat = async (chatId) => {
+  const messages = await messageModel.findAll({
+    where: {
+      chatId
+    }
+  })
+  if (!messages) {
+    throw new Error('An error occured retreiving messages')
+  }
+  return messages
+}
+
+export const createUser = async (email, username, password) => {
+  const newUser = await User.create({
+    email,
+    username,
+    password
+  })
+  if (!newUser) {
+    throw new Error('An error occured creating the user')
+  }
+  return newUser
+}
+
+// need to figure out ho to update the json blob here
+export const addUserToChat = async (userId, chatId) => {
+  const chat = await chatModel.findOne({
+    where: {
+      id: chatId
+    }
+  })
+  // TO DO - check that user is not already in the chat
+  const updatedUsers = [...chat.users, userId]
+  const updatedChat = chatModel.update({users: updatedUsers}, {where: {
+    id: chatId
+  }})
+  if (!updatedChat) {
+    return new Error('There was an error adding the User to the Chat')
+  }
+  return updatedChat
+}
+
+export const getAllChatMessager = async (chatId) => {
+  const messages = await messageModel.findAll({where: {
+    chatId
+  }})
+  if (!messages) {
+    return new Error('There was an error retreiving the messages')
+  }
+  return messages
+}
+
+export const addMessageToCgat = async (message, chatId, userId) => {
+  const updatedMessages = messageModel.create({
+    message,
+    chatId,
+    userId
+  })
+  if (!updatedMessages) {
+    return new Error('There was an error adding a new message to the chat')
+  }
+  return updatedMessages
 }
