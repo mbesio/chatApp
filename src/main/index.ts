49b2da804b19b3624d2d99a7ae43e2ffa25c14d6
Matteo Besio <matteo.besio@gmail.com>
2022-05-07 22:35:38 -0700

    progressing with the prisma refactor

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,25 @@
-import server from './server'
+import { ApolloServer } from 'apollo-server'
+import { PrismaClient } from '@prisma/client'
+
+import typeDefs from './schema/schema'
+import resolvers from './resolvers/resolvers'
+
+const prisma = new PrismaClient()
+
+export interface Context {
+  prisma: PrismaClient
+
+}
+const context : Context = {
+  prisma: prisma,
+}
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context,
+})
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
